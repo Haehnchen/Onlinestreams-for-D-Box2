2973b0f19caea650b373bea4e93cd13dd1c361a4
Haehnchen <espendiller@gmx.de>
2009-12-12 09:53:13 +0000

    fixed searching for myvideo.de

--- scripts/MyVideo.php
+++ scripts/MyVideo.php
@@ -1,27 +1,77 @@
 <?
 /*
-<changelog>fixed flvdownload 11.02.09</changelog>
+<changelog>fixed: searching dec 09</changelog>
 <info>use csv to add searchterms</info>
 */
-$search="http://www.myvideo.de/news.php?lpage=%index%&rubrik=uoijv&searchWord=%string%&searchChannel=&searchOrder=%orderby%";
 
-$seite['Seite 1']['nr']=1;
-$seite['Seite 2']['nr']=2;
-$seite['Seite 3']['nr']=3;
-$seite['Seite 4']['nr']=4;
-$seite['Seite 5']['nr']=5;
-$seite['Seite 6']['nr']=6;
-$seite['Seite 7']['nr']=7;
-$seite['Seite 8']['nr']=8;
-$seite['Seite 9']['nr']=9;
-$seite['Seite 10']['nr']=10;
+$search='http://www.myvideo.de/Videos_A-Z?lpage=%page%&searchWord=%string%&searchOrder=%orderby%';
+
+for ($i=0;$i<13;$i++) $seite['Seite '.($i + 1)]['nr']=$i;
 
 $orderby['Relevanz']['api']="0";
 $orderby['Datum']['api']="1";
 $orderby['Zugriffe']['api']="5";
 $orderby['Bewertung']['api']="2";
 
-function dlflv($url) {
+
+function getdir() {
+	global $links,$orderby;
+	$r=split("/",trim($_GET['dir'],"/"));
+
+	if (count($r)==2) {
+		return gennavi($links);
+	}
+
+	if (count($r)==3) {
+		$ar=$links[$r[2]];
+		if (isset($ar['suche'])) {
+			return gennavi($orderby);
+		} else {
+			return gennavi(myvideo_rss_feed_input($ar['url']));
+		}
+	}
+
+	if (count($r)==4) {
+		return gennavi($seite);
+	}
+
+	if (count($r)==5) {
+		$ar=$links[$r[2]];
+		$ar=myvideo_search_input(urlencode($ar['suche']),$page,$orderby[$r[3]]['api']);
+		return gennavi($ar);
+	}
+}
+
+function geturl($pfad) {
+	global $links,$orderby;
+	$r=split("/",trim($pfad,"/"));
+
+	#check if we are searching
+	if(count($r)==6) {
+		$page=$seite[$r[4]]['nr'];
+		$ar=myvideo_search_input(urlencode($links[$r[2]]['suche']),$page,$orderby[$r[3]]['api']);
+		$key=$r[5]; 
+		return myvideo_flv_download($ar[$key]['url']);
+	}
+
+
+	#no searchobject, use rss
+	$in=myvideo_rss_feed_input($links[$r[2]]['url']);
+	$t=stripslashes($r[3]);
+	return myvideo_flv_download($in[$t]['url']);
+}
+
+/**
+ * Parse the video url of a given myvideo video link site
+ * and returns the http url of the video
+ * * 
+ * @param string $url
+ *   url of video must contain watch/*
+ *   http://www.myvideo.de/watch/7125794
+ * @return string
+ *   the url of the video as http url; should the flv file
+ */
+function myvideo_flv_download($url) {
 	preg_match('|watch/(\d+)/|i', $url, $match);
 	$objekt=new Browser($browser);
 	if ($cookies!="") $objekt->cookies_set($cookies);
@@ -32,7 +82,16 @@ function dlflv($url) {
 	return urldecode($matches[1]).".flv";
 }
 
-function rssinput($url) {
+
+/**
+ * parse the myvideo feeds
+ *
+ * @param string $url
+ *   feed url
+ * @return array
+ *   videos as array
+ */
+function myvideo_rss_feed_input($url) {
 	$t_html = cacheurl($url);
 	preg_match_all('/<item>(.*?)<\/item>/si',$t_html,$row);
 
@@ -46,8 +105,26 @@ function rssinput($url) {
 	return $out;
 }
 
-function input_search($url) {
+/**
+ * peforms a search on myvideo and returns the videos as array
+ *
+ * @param string $string
+ *   the searchstring
+ * @param string $page
+ *   the page of the results to display
+ * @param string $orderby
+ *   order the results: relevance=0,date=1,views=5
+ * @return array
+ *   returns videopages as array
+ */
+function myvideo_search_input($string,$page=0,$orderby=0) {
+	global $search;	
+	$url= str_replace("%string%",urlencode($string),$search);
+	$url= str_replace("%orderby%",$orderby,$url);
+	$url= str_replace("%page%",$page,$url);
+
 	$t_html = cacheurl($url);
+	$out=array();
 	preg_match_all("|<span class='title'>(.*?)</span>|si",$t_html,$row);
 	foreach ($row[0] as $mov) {
 	         preg_match("/title='(.*?)'/is", $mov, $matches); $tmp_array['title'] = reducehtml($matches[1]);
@@ -57,62 +134,5 @@ function input_search($url) {
 	}
 	return $out;
 }
-
-
-function getdir() {
-	global $links,$orderby,$search,$seite;
-	$r=split("/",trim($_GET['dir'],"/"));
-
-	if (count($r)==2) {
-		return gennavi($links);
-	}
-
-	if (count($r)==3) {
-		$ar=$links[$r[2]];
-		if (isset($ar['suche'])) {
-			return gennavi($orderby);
-		} else {
-			return gennavi(rssinput($ar['url']));
-		}
-	}
-
-	if (count($r)==4) {
-		return gennavi($seite);
-	}
-
-	if (count($r)==5) {
-		$ar=$links[$r[2]];
-		$index=$seite[$r[4]]['nr'];
-		$ur= str_replace("%string%",urlencode($ar['suche']),$search);
-		$ur= str_replace("%orderby%",$orderby[$r[3]]['api'],$ur);
-		$ur= str_replace("%index%",$index,$ur);
-		return gennavi(input_search($ur,implode("/",$r)));
-	}
-}
-
-function geturl($pfad) {
-
-	global $links,$seite,$oderby,$search;
-	$r=split("/",trim($pfad,"/"));
-
-	#youtube suche
-	if(count($r)==6) {
-		$index=$seite[$r[4]]['nr'];
-		$ur= str_replace("%string%",urlencode($links[$r[2]]['suche']),$search);
-		$ur= str_replace("%orderby%",$orderby[$r[3]]['api'],$ur);
-		$ur= str_replace("%index%",$index,$ur);
-		$ar=input_search($ur,implode("/",$r));
-		#d_write("fav/".md5(implode("/",$r))," ");
-		$key=$r[5]; #if (!isset($ar[$key])) $key=substr($key,1);
-		return dlflv($ar[$key]['url']);
-	}
-
-
-	#no searchobject, use rss
-	$in=rssinput($links[$r[2]]['url']);
-	$t=stripslashes($r[3]);
-	return dlflv($in[$t]['url']);
-}
-
 
 ?>
